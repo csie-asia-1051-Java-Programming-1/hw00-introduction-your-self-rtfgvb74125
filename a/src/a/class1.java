1b88007f5298d2528613ffc696e128f4ea1d9a17
rtfgvb74125 <105021056@live.asia.edu.tw>
2016-09-12 15:50:29 +0800

    class01
    01

---
+++ a/src/a/class1.java
@@ -0,0 +1,12 @@
+package a;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("105021056");
+System.out.println("WangJiaEn");
+System.out.println("資工數位1A");
+	}
+
+}
